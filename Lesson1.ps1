44b1b07e71a2e11f4697b997fc491104fcd7733a
sathishdsgithub <sathish.damodaran.suresh@gmail.com>
2020-02-21 21:07:20 -0800

    Add files via upload

---
+++ Lesson1.ps1
@@ -0,0 +1,7 @@
+﻿$firstname = $args[0]
+$lastname = $args[1]
+
+Write-Host "Hello $firstname $lastname"
+Write-Host "Your login from $env:COMPUTERNAME"
+write-host "Thank you"
+write-host "Im using the shell $env:ComSpec"
---
+++ Using Param.ps1
@@ -0,0 +1,4 @@
+﻿param($firstname,$lastname)
+write-host "Hello $firstname $lastname"
+write-host "You are using the windows shell which is at the location $env:ComSpec"
+
